4f958cb15e5d21a021aa4c69c399054674207834
Nick <nick@kousu.ca>
2022-04-09 01:46:43 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,26 @@ import { fetchInstallationToken } from "./fetch-installation-token.js";
 
 const run = async () => {
   try {
+    info("Getting inputs");
     const appId = getInput("app_id", { required: true });
     const privateKeyInput = getInput("private_key", { required: true });
+    info("got private key");
     const privateKey = isBase64(privateKeyInput)
       ? Buffer.from(privateKeyInput, "base64").toString("utf8")
       : privateKeyInput;
+    info("converted private key to binary");
 
     const installationId = getInput("installation_id");
     const repositoryInput = getInput("repository");
+    info("installationId = " + installationId);
+    info("repositoryInput = " + repositoryInput);
     const [owner, repo] = repositoryInput
       ? repositoryInput.split("/")
       : [context.repo.owner, context.repo.repo];
+    info("owner = " + owner);
+    info("repo = " + repo);
 
+    info("trying to get token");
     const installationToken = await fetchInstallationToken({
       appId,
       installationId: installationId ? Number(installationId) : undefined,
@@ -25,6 +33,7 @@ const run = async () => {
       privateKey,
       repo,
     });
+    info("got token");
 
     setSecret(installationToken);
     setOutput("token", installationToken);
